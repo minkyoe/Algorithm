c500b11d6d0fb42a3ca875183ced35576a90e729
minkyoe <ddal0924@naver.com>
2020-07-26 13:52:27 +0900

    [Baekjoon/문자열] 1316_그룹 단어 체커(SOLVED! 다시 풀어보기)

--- Baekjoon/1316_그룹단어체커.cpp
+++ Baekjoon/1316_그룹단어체커.cpp
@@ -11,8 +11,7 @@ int ans;
 int main(void){
     
     cin >> N;
-   
-    ans = N;
+    ans = 0;
 
     for (int i=0; i<N; i++) {
         string str = "";
@@ -20,59 +19,22 @@ int main(void){
         memset(alphaCheck, 0, sizeof(alphaCheck));
 
         int size = str.length();
-        for (int j=1; j<size; j++) {
-            string s1 = str.substr(j-1,1);
-            string s2 = str.substr(j,1);
-
-            int alphaIdx = stoi(s2.c_str());
-            cout << alphaIdx << endl;
-            if (s1 != s2) {
-                if (alphaCheck[alphaIdx]) {
-                    ans --;
+        alphaCheck[str[0]-97] = 1;
+        int j = 1;
+        for (j=1; j<size; j++) {
+            if (str[j-1] != str[j]) {
+                if (alphaCheck[str[j]-97] == 0) {
+                    alphaCheck[str[j]-97]++;
+                } else {
                     break;
                 }
-                alphaCheck[alphaIdx] = 1;
             }
-           
+        }
+        if (j == size) {
+            ans ++;
         }
     }
-
     
-    // memset(a, 0, sizeof(a));
-    // ans = N;
-
-    // for (int i=0; i<N; i++) {
-    //     cin >> a[i];
-    // }
-
-    // for (int aIdx=0; aIdx<N; aIdx++) {
-    //     string str = "";
-    //     cin >> str;
-    //     int isNotFlag = false;
-    //     cout << "str: " << str << endl;
-
-    //     for (int i=0; i<str.length(); i++) { 
-    //         string s = str.substr(i,1);
-
-    //         for (int j=0; j<str.length(); j++) {
-    //             if (j == i) continue;
-    //             string s2 = str.substr(j,1);
-    //             int tmp = (j > idx) ? j-idx : idx-j;
-                
-    //             if (s == s2 && tmp > 1) {
-    //                 cout << s <<" " << s2 << endl;
-    //                 cout << "tmp: " << tmp << endl;
-    //                 isNotFlag = true;
-    //             }
-    //         }
-    //         if (isNotFlag) {
-    //             ans--;
-    //             break;
-    //         }
-    //     }
-    // }
-    
-
     cout << ans;
 
     return 0;

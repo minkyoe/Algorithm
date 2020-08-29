3eb7e98236755ee09f8a5d04ffd4b41e2f49bb8d
minkyoe <ddal0924@naver.com>
2020-08-29 20:23:43 +0900

    [Baekjoon(JAVA)/슬라이딩윈도우] 10025_게으른 백곰 (주석 추가)

--- Baekjoon/Boj10025_게으른백곰.java
+++ Baekjoon/Boj10025_게으른백곰.java
@@ -12,6 +12,7 @@ public class Main_10025_게으른백곰 {
 	private static int move;
 	private static int cnt;
 	private static int maxCnt;
+	private static int maxPoint;
 
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(bf.readLine(), " ");
@@ -21,20 +22,25 @@ public class Main_10025_게으른백곰 {
 		map = new int[MAX];
 		cnt = 0; // 구간에 있는 얼음 합
 		maxCnt = 0;
+		maxPoint = 0;
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(bf.readLine(), " ");
 			int ice = Integer.parseInt(st.nextToken());
 			int point = Integer.parseInt(st.nextToken());
+			maxPoint = maxPoint < point ? point : maxPoint; // 양동이 좌표 최대 값
 			map[point] = ice;
 		}
 
-		for (int i = 0; i <= move && i < MAX; i++) {
+		for (int i = 0; i <= move && i <= maxPoint; i++) {
 			cnt += map[i];
 		}
 		maxCnt = maxCnt < cnt ? cnt : maxCnt;
-
-		for (int start = 1; start < MAX - move; start++) {
+		
+		// move >= maxPoint -> 곰의 이동 가능한 거리가 양동이 좌표 최대값보다 크다는 것은 구간합에서 이미 존재하는 얼음을 다 더했다는 뜻이므로 
+		//					  얼음을 다 더한 값이 최대값임
+		
+		for (int start = 1; start <= maxPoint - move; start++) {
 			// 앞에 있는 원소 빼고
 			cnt -= map[start - 1];
 

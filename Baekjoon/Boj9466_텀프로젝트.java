856ec0c8531644d160b4d85d63e6ca87110dcb77
minkyoe <ddal0924@naver.com>
2020-11-10 13:58:34 +0900

    [Baekjoon(JAVA)/DFS] 9466_텀프로젝트 (사이클 개수 세기, 복습하자⭐️)

--- Baekjoon/Boj9466_텀프로젝트.java
+++ Baekjoon/Boj9466_텀프로젝트.java
@@ -8,9 +8,9 @@ import java.util.StringTokenizer;
 public class Main_9466_텀프로젝트2 {
 	private static int N;
 	private static int[] arr;
-	private static boolean[] selected;
-	private static int notSelected;
+	private static boolean[] done;
 	private static boolean[] visited;
+	private static int cnt;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -19,47 +19,36 @@ public class Main_9466_텀프로젝트2 {
         for (int testCase = 1; testCase <= tc; testCase++) {
 			N = Integer.parseInt(bf.readLine()); // 학생 수 
 			arr = new int[N+1]; // 0번 안씀
-			selected = new boolean[N+1]; // 0번 안씀
-			notSelected = N; // 프로젝트 팀에 속하지 못한 학생들의 수
+			cnt = 0;
+			done = new boolean[N+1]; // 0번 안씀
+			visited = new boolean[N+1];
 			
 			StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
 			for (int i = 1; i <= N; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
-				if (i == arr[i]) {
-					selected[i] = true;
-					--notSelected;
-				}
 			}
 			
-			visited = new boolean[N+1];
 			for (int i = 1; i <= N; i++) {
-				if(selected[i]) continue;
-				visited[i] = true;
-				if(dfs(i, i)) {
-					selected[i] = true;
-				}
+				if(!visited[i]) dfs(i);
 			}
 			
-			System.out.println(notSelected);
+			System.out.println(N-cnt);
 		} // end of tc
 	} // end of main
 
-	private static boolean dfs(int idx, int root) {
-		if (selected[arr[idx]] || visited[arr[idx]]) {
-			return false;
-		} else {
-			if (arr[idx] == root) {
-				--notSelected;
-				selected[idx] = true;
-				return true;
-			}
-			visited[idx] = true;
-			if (dfs(arr[idx], root)) {
-				--notSelected;
-				selected[idx] = true;
-				return true;
+	private static void dfs(int num) {
+		visited[num] = true;
+		int next = arr[num];
+		
+		if (!visited[next]) dfs(next);
+		else {
+			if (!done[next]) {
+				for (int i = next; i != num; i = arr[i]) {
+					cnt++;
+				}
+				cnt++; // 자기자신
 			}
 		}
-		return false;
+		done[num] = true;
 	}
 }

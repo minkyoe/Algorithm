008f95fe56df5e69d6ed1f08354a798b4d9d89eb
minkyoe <ddal0924@naver.com>
2021-01-12 22:29:20 +0900

    [Baekjoon(JAVA)/우선순위큐] 2075_N번째 큰 수

---
+++ Baekjoon/Boj2075_N번째큰수2.java
@@ -0,0 +1,34 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main_2075_N번째큰수2 {
+
+	private static int N;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(bf.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+			for (int j = 0; j < N; j++) {
+				pq.offer(Integer.parseInt(st.nextToken()));
+			}
+		}
+		
+		int cnt = 1;
+		while(cnt++ < N) {
+			pq.poll();
+		}
+		
+		System.out.println(pq.peek());
+	}
+
+}

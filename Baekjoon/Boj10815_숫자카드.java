01b4715e25e71011128790f4f0d164e7f64c5869
minkyoe <ddal0924@naver.com>
2021-01-17 18:21:16 +0900

    [Baekjoon(JAVA)/이분탐색] 10815_숫자 카드

--- Baekjoon/Boj10815_숫자카드.java
+++ Baekjoon/Boj10815_숫자카드.java
@@ -1,17 +1,14 @@
 package Baekjoon;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_10815_숫자카드 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(bf.readLine());
 		StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
@@ -36,22 +33,20 @@ public class Main_10815_숫자카드 {
 				int midIdx = (left + right) / 2;
 				int mid = nums[midIdx];
 				if (mid < n)
-					left++;
+					left = midIdx + 1;
 				else if (mid > n)
-					right--;
+					right = midIdx - 1;
 				else {
 					isRight = true;
 					break;
 				}
 			}
 
-			if (isRight) bw.write("1 ");
-			else bw.write("0 ");
+			if (isRight) sb.append("1 ");
+			else sb.append("0 ");
 		}
 
-//		System.out.println(sb);
-		bw.close();
-
+		System.out.println(sb);
 	} // end of main
 
 }

9eb2996167c45b912549f09c9cf2d6da797f8a92
kimmk0924 <kimmk0924@gmail.com>
2021-02-07 23:22:34 +0900

    [Baekjoon(JAVA)/수학] 13171_A

--- Baekjoon/Main_13171_A.java
+++ Baekjoon/Main_13171_A.java
@@ -6,43 +6,33 @@ public class Main_13171_A {
 
 	private static int A;
 	private static int X;
-	private static int[] arr;
+	private static long[] arr;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		A = Integer.parseInt(bf.readLine());
 		X = Integer.parseInt(bf.readLine());
-		arr = new int[65];
+		arr = new long[65];
 		
-		int idx = 1;
-		int s = 0;
-		int r = (int) Math.pow(2, s);
-		while(r <= X) {
-			int num = 1;
-			for (int i = 0; i < r; i++) {
-				num *= A;
-			}
-			arr[idx++] = num;
-			s++;
-			r = (int) Math.pow(2, s);
+		arr[0] = A % 1000000007;
+		for (int i = 1; i < 64; i++) {
+			arr[i] = ((arr[i-1] * arr[i-1]) % 1000000007); 
 		}
 		
-		for (int i = 0; i < 10; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
+//		for (int i = 0; i < 64; i++) {
+//			System.out.print(arr[i] + " ");
+//		}
+//		System.out.println();
 		
 		long ans = 1;
-		while (s > 0 && X > 0) {
-			System.out.println("s  " + s);
-			System.out.println("arr[s] " + (arr[s] % 1000000007));
-			ans *= (arr[s] % 1000000007);
-			System.out.println("ans " + ans );
-			System.out.println("------");
-			X -= s;
-			--s;
+		int count = 0;
+		while (X != 0) {
+			if (X%2 == 1) {
+				ans = (ans * (arr[count] % 1000000007)) % 1000000007; 
+			}
+			X /= 2;
+			count++;
 		}
-
 		System.out.println(ans);
 	} // end of main
 

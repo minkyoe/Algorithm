346dcb94735fc8e2b027e11aaf09af026f67d1a8
minkyoe <ddal0924@naver.com>
2021-03-09 21:08:43 +0900

    [Programmers(JAVA)/구현] 월간 코드 챌린지 시즌1_두개 뽑아서 더하기

---
+++ Programmers/월간코드챌린지1/두개뽑아서더하기.java
@@ -0,0 +1,29 @@
+package 월간코드챌린지1;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[10001];    
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i == j) continue;
+                int sum = numbers[i] + numbers[j];
+                if (list.contains(sum)) continue;
+                list.add(sum);
+            }
+        }
+        
+        Collections.sort(list);
+        
+        int[] realAnswer = new int[list.size()];
+        int idx = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            realAnswer[idx++] = list.get(i);
+        }
+        return realAnswer;
+    }
+}
